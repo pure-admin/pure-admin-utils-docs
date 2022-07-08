38f9dfc2d611155016346f1f76b2e50fc5ee3df8
xiaoxian521 <1923740402@qq.com>
2022-07-08 11:46:56 +0800

    docs: update

--- docs/hooks/useWatermark/useWatermark.md
+++ docs/hooks/useWatermark/useWatermark.md
@@ -1,4 +1,20 @@
+<script setup>
+import watermark from './watermark.vue'
+</script>
+
 # useWatermark
 <ClientOnly>
   <description description="useWatermark" /> 
 </ClientOnly>
+
+## 基础用法
+<ClientOnly>
+  <watermark />
+</ClientOnly>
+<details>
+
+<summary>查看代码</summary>
+
+<<< @/hooks/useWatermark/watermark.vue
+
+</details>
---
+++ docs/hooks/useWatermark/watermark.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { NButton, NSpace } from "naive-ui";
+import { useWatermark } from "@pureadmin/utils";
+
+let color = ref("#409EFF");
+let value = ref("pure-admin-utils");
+const { setWatermark, clear } = useWatermark();
+</script>
+
+<template>
+  <naive-theme>
+    <n-space>
+      <n-button @click="setWatermark(value, { fillStyle: color })">
+        创建水印
+      </n-button>
+      <n-button @click="clear"> 清除水印 </n-button>
+    </n-space>
+  </naive-theme>
+</template>
