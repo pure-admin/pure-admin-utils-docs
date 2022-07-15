af40160d773181c48f7e01d8da507cb05a862a2a
xiaoxian521 <1923740402@qq.com>
2022-07-15 13:22:57 +0800

    docs: update

---
+++ docs/utils/substring/index.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+import { ref } from "vue"
+import { NButton, NSpace, NGradientText } from "naive-ui"
+import { subBefore, subAfter, subBothSides, subBetween, subTextAddEllipsis, splitNum } from "@pureadmin/utils"
+
+let character1 = ref('')
+let character2 = ref('')
+let character3 = ref()
+let character4 = ref('')
+let character5 = ref('')
+let character6 = ref()
+</script>
+
+<template>
+  <naive-theme>
+    <n-space className="flex items-center mb-2">
+      <n-button @click="character1 = subBefore('hello,gird', ',')"> 截取指定字符前面的值 </n-button>
+      <n-gradient-text type="info">
+        原字符：hello,gird
+      </n-gradient-text>
+      <n-gradient-text type="error" v-show="String(character1).length > 0">
+        截取后字符：{{ character1 }}
+      </n-gradient-text>
+    </n-space>
+
+    <n-space className="flex items-center mb-2">
+      <n-button @click="character2 = subAfter('hello,gird', ',')"> 截取指定字符后面的值 </n-button>
+      <n-gradient-text type="info">
+        原字符：hello,gird
+      </n-gradient-text>
+      <n-gradient-text type="error" v-show="String(character2).length > 0">
+        截取后字符：{{ character2 }}
+      </n-gradient-text>
+    </n-space>
+
+    <n-space className="flex items-center mb-2">
+      <n-button @click="character3 = subBothSides('hello,gird', ',')"> 截取指定字符两边的值 </n-button>
+      <n-gradient-text type="info">
+        原字符：hello,gird
+      </n-gradient-text>
+      <n-gradient-text type="error" v-show="character3?.length > 0">
+        截取后字符：{{ character3 }}
+      </n-gradient-text>
+    </n-space>
+
+    <n-space className="flex items-center mb-2">
+      <n-button @click="character4 = subBetween('i love you', 'i ', ' you')"> 截取指定两个字符之间的值 </n-button>
+      <n-gradient-text type="info">
+        原字符：i love you
+      </n-gradient-text>
+      <n-gradient-text type="error" v-show="character4?.length > 0">
+        截取后字符：{{ character4 }}
+      </n-gradient-text>
+    </n-space>
+
+    <n-space className="flex items-center mb-2">
+      <n-button @click="character5 = subTextAddEllipsis('hello,gird', 5)"> 截取字符并追加省略号（常用场景：echarts） </n-button>
+      <n-gradient-text type="info">
+        原字符：hello,gird
+      </n-gradient-text>
+      <n-gradient-text type="error" v-show="character5?.length > 0">
+        截取后字符：{{ character5 }}
+      </n-gradient-text>
+    </n-space>
+
+    <n-space className="flex items-center mb-2">
+      <n-button @click="character6 = splitNum(123456)"> 将数字拆分为单个数字组成的数组 </n-button>
+      <n-gradient-text type="info">
+        原数字：123456
+      </n-gradient-text>
+      <n-gradient-text type="error" v-show="character6?.length > 0">
+        拆分后的数字：{{ character6 }}
+      </n-gradient-text>
+    </n-space>
+  </naive-theme>
+</template>
--- docs/utils/substring/substring.md
+++ docs/utils/substring/substring.md
@@ -1,4 +1,21 @@
+<script setup>
+import Index from './index.vue'
+</script>
+
 # substring
+
 <ClientOnly>
-  <description :tagNameList="['浏览器','Node']" description="substring" /> 
+  <description :tagNameList="['浏览器','Node']" description="截取字符相关函数" /> 
 </ClientOnly>
+
+## 基础用法
+<ClientOnly>
+  <Index />
+</ClientOnly>
+<details>
+
+<summary>查看代码</summary>
+
+<<< @/utils/substring/index.vue
+
+</details>
