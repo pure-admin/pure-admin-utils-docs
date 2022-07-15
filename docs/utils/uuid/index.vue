ebb78ff4c8a0995c797059da10c4e8ae53a31786
xiaoxian521 <1923740402@qq.com>
2022-07-15 10:39:44 +0800

    docs: update

---
+++ docs/utils/uuid/index.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { ref } from "vue"
+import { NButton, NSpace, NGradientText, NTag } from "naive-ui"
+import { buildUUID, buildPrefixUUID, uuid } from "@pureadmin/utils"
+
+let UUID = ref('')
+let customUUID = ref('')
+let prefixUUID = ref('')
+</script>
+
+<template>
+  <naive-theme>
+    <n-space className="flex items-center mb-2">
+      <n-button @click="UUID = buildUUID()"> 32位uuid </n-button>
+      <n-gradient-text type="info">
+        {{ UUID }}
+        <n-tag :bordered="false" type="info" v-show="UUID.length > 0">
+          长度：{{ UUID.length }}
+        </n-tag>
+      </n-gradient-text>
+    </n-space>
+
+    <n-space className="flex items-center mb-2">
+      <n-button @click="prefixUUID = buildPrefixUUID('I love you, simpleton-')"> 自定义前缀的uuid </n-button>
+      <n-gradient-text type="error">
+        {{ prefixUUID }}
+        <n-tag :bordered="false" type="error" v-show="prefixUUID.length > 0">
+          长度：{{ prefixUUID.length }}
+        </n-tag>
+      </n-gradient-text>
+    </n-space>
+
+    <n-space className="flex items-center mb-2">
+      <n-button @click="customUUID = uuid(32, 62, 'simpleton-')"> 指定长度和基数的uuid </n-button>
+      <n-gradient-text type="error">
+        {{ customUUID }}
+        <n-tag :bordered="false" type="error" v-show="customUUID.length > 0">
+          长度：{{ customUUID.length }}
+        </n-tag>
+      </n-gradient-text>
+    </n-space>
+  </naive-theme>
+</template>
--- docs/utils/uuid/uuid.md
+++ docs/utils/uuid/uuid.md
@@ -1,4 +1,26 @@
+<script setup>
+import Index from './index.vue'
+</script>
+
 # uuid
+
 <ClientOnly>
-  <description :tagNameList="['浏览器','Node']" description="uuid" /> 
+  <description :tagNameList="['浏览器','Node']" description="生成uuid" /> 
 </ClientOnly>
+
+## 基础用法
+<ClientOnly>
+  <Index />
+</ClientOnly>
+<details>
+
+<summary>查看代码</summary>
+
+<<< @/utils/uuid/index.vue
+
+</details>
+
+::: tip
+指定基数的母体为：`0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz`，总长度`62`  
+指定基数也就是规定要生成的`uuid`采用哪些字符组合，比如基数为`10`，那么生成的`uuid`就会采用基数母体的前`10`位进行组合，也就是`0123456789`
+:::
