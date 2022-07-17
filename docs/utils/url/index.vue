daaf78a1ba3b70d07c29004d7aa813a53ebc4513
xiaoxian521 <1923740402@qq.com>
2022-07-17 11:42:25 +0800

    docs: update

---
+++ docs/utils/url/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from "vue"
+import { NGradientText } from "naive-ui"
+import { getQueryMap, getLocation } from "@pureadmin/utils"
+
+let url = ref('https://test?a=test&&token=token')
+</script>
+
+<template>
+  <naive-theme>
+    <p>url：{{ url }}</p>
+
+    <n-gradient-text type="info">
+      url中所有参数：{{ getQueryMap(url) }}
+    </n-gradient-text>
+
+    <p>当前的location信息：<n-gradient-text type="info">
+        {{ getLocation() }}
+      </n-gradient-text>
+    </p>
+  </naive-theme>
+</template>
--- docs/utils/url/url.md
+++ docs/utils/url/url.md
@@ -1,4 +1,21 @@
+<script setup>
+import Index from './index.vue'
+</script>
+
 # url
+
 <ClientOnly>
-  <description :tagNameList="['浏览器']" description="url" /> 
+  <description :tagNameList="['浏览器']" description="提取url中所有参数、获取当前的location信息" /> 
 </ClientOnly>
+
+## 基础用法
+<ClientOnly>
+  <Index />
+</ClientOnly>
+<details>
+
+<summary>查看代码</summary>
+
+<<< @/utils/url/index.vue
+
+</details>
