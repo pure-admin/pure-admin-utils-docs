df1aba2bae7f2266abd4a6af4d359dbf757abdca
xiaoxian521 <1923740402@qq.com>
2022-07-17 10:00:03 +0800

    docs: update

---
+++ docs/utils/nameTransform/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from "vue"
+import { NButton, NSpace, NGradientText } from "naive-ui"
+import { nameCamelize, nameHyphenate } from "@pureadmin/utils"
+
+let name = ref('i-love-study')
+</script>
+
+<template>
+  <naive-theme>
+    <n-space className="flex items-center mb-2">
+      <n-button @click="name = nameCamelize(name)"> 横线转驼峰命名 </n-button>
+      <n-button @click="name = nameHyphenate(name)"> 驼峰命名转横线命名 </n-button>
+      <n-gradient-text type="info">
+        {{ name }}
+      </n-gradient-text>
+    </n-space>
+  </naive-theme>
+</template>
--- docs/utils/nameTransform/nameTransform.md
+++ docs/utils/nameTransform/nameTransform.md
@@ -1,4 +1,25 @@
+<script setup>
+import Index from './index.vue'
+</script>
+
 # nameTransform
+
 <ClientOnly>
-  <description :tagNameList="['浏览器','Node']" description="nameTransform" /> 
+  <description :tagNameList="['浏览器','Node']" description="横线、驼峰命名互转" /> 
 </ClientOnly>
+
+## 基础用法
+<ClientOnly>
+  <Index />
+</ClientOnly>
+<details>
+
+<summary>查看代码</summary>
+
+<<< @/utils/nameTransform/index.vue
+
+</details>
+
+::: tip
+驼峰命名转横线命名：拆分字符串，使用 `-` 相连，并且全部转换为小写
+:::
