820a0cfe07d11ce9bd3689292d8b494db9edda23
xiaoxian521 <1923740402@qq.com>
2022-07-19 16:23:03 +0800

    docs: update

---
+++ docs/utils/convertPath/convertPath.md
@@ -0,0 +1,21 @@
+<script setup>
+import Index from './index.vue'
+</script>
+
+# convertPath
+
+<ClientOnly>
+  <description :tagNameList="['Node']" description="将Windows反斜杠路径转换为斜杠路径" /> 
+</ClientOnly>
+
+## 基础用法
+<ClientOnly>
+  <Index />
+</ClientOnly>
+<details>
+
+<summary>查看代码</summary>
+
+<<< @/utils/convertPath/index.vue
+
+</details>
---
+++ docs/utils/convertPath/index.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { ref } from "vue"
+import { convertPath, clone } from "@pureadmin/utils"
+import { NCard, NGradientText, NSpace } from "naive-ui"
+
+let path1 = ref('Documents\\newFolder')
+let path2 = ref('C:\\Documents\\newFolder\\test.js')
+</script>
+
+<template>
+  <naive-theme>
+    <n-space className="flex items-center mb-2">
+      <n-card title="Demo1" embedded :bordered="false">
+        <n-gradient-text type="info">
+          windows路径：{{ clone(path1) }}
+        </n-gradient-text>
+        <br />
+        <n-gradient-text type="error">
+          转换后的路径：{{ convertPath(path1) }}
+        </n-gradient-text>
+      </n-card>
+    </n-space>
+
+    <n-space className="flex items-center mb-2">
+      <n-card title="Demo2" embedded :bordered="false">
+        <n-gradient-text type="info">
+          windows路径：{{ clone(path2) }}
+        </n-gradient-text>
+        <br />
+        <n-gradient-text type="error">
+          转换后的路径：{{ convertPath(path2) }}
+        </n-gradient-text>
+      </n-card>
+    </n-space>
+  </naive-theme>
+</template>
