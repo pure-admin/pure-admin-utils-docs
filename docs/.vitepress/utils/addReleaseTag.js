45c1c16d4932cd0d99c4fd464420f1b6fbb87c43
xiaoxian521 <1923740402@qq.com>
2022-10-16 13:07:50 +0800

    chore: update

--- docs/.vitepress/utils/addReleaseTag.js
+++ docs/.vitepress/utils/addReleaseTag.js
@@ -2,6 +2,7 @@ import { version } from "../../../package.json"
 
 export function addReleaseTag() {
   const tagLineParagragh = document.querySelector('div.VPHero.has-image.VPHomeHero > div > div.main > p.tagline')
+  if (document.querySelector('samp')) return
   const docsReleaseTagSpan = document.createElement('samp')
   docsReleaseTagSpan.classList.add('github-release-tag')
   docsReleaseTagSpan.innerText = `v${version}`
