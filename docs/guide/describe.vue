a65e01fe957ede2a0fdc3fe117b77bee4e4b34db
xiaoxian521 <1923740402@qq.com>
2022-10-18 02:00:18 +0800

    docs: update

--- docs/guide/describe.vue
+++ docs/guide/describe.vue
@@ -14,7 +14,8 @@ const { hooksLen, utilsLen } = useData()
           <n-number-animation :from="0" :to="hooksLen" /> 个
           <n-tooltip trigger="hover">
             <template #trigger>
-              <n-button text type="primary" @click="go('/hooks/useCopyToClipboard/useCopyToClipboard')">
+              <n-button text type="primary"
+                @click="go('/pure-admin-utils-docs/hooks/useCopyToClipboard/useCopyToClipboard')">
                 Hooks
               </n-button>
             </template>
@@ -24,7 +25,7 @@ const { hooksLen, utilsLen } = useData()
           <n-number-animation :from="0" :to="utilsLen" /> 个
           <n-tooltip trigger="hover">
             <template #trigger>
-              <n-button text type="primary" @click="go('/utils/banMouseEvent/banMouseEvent')">
+              <n-button text type="primary" @click="go('/pure-admin-utils-docs/utils/banMouseEvent/banMouseEvent')">
                 Utils
               </n-button>
             </template>
