1b006e19a65adbca09c619980f0a482a81ab627a
xiaoxian521 <1923740402@qq.com>
2022-12-03 13:18:16 +0800

    docs: update

--- docs/utils/debounce/debounce.vue
+++ docs/utils/debounce/debounce.vue
@@ -10,7 +10,17 @@ const handle = () => {
 
 const immediateDebounce = debounce(handle, 1000, true)
 
+const immediateDebounceParams: any = debounce((first, second) => {
+  message.success(first)
+  message.success(second)
+}, 1000, true)
+
 const debounceClick = debounce(handle, 1000)
+
+const debounceClickParams: any = debounce((first, second) => {
+  message.success(first)
+  message.success(second)
+}, 1000)
 </script>
 
 <template>
@@ -19,8 +29,14 @@ const debounceClick = debounce(handle, 1000)
       <n-button @click="immediateDebounce">
         连续点击我，只会执行第一次点击事件，立即执行
       </n-button>
+      <n-button @click="immediateDebounceParams('我是传过来的第一个参数', '我是传过来的第二个参数')">
+        连续点击我，只会执行第一次点击事件，立即执行（可传参写法，不限制参数数量）
+      </n-button>
       <n-button @click="debounceClick">
         连续点击我，只会执行最后一次点击事件，延后执行
+      </n-button>
+      <n-button @click="debounceClickParams('我是传过来的第一个参数', '我是传过来的第二个参数')">
+        连续点击我，只会执行最后一次点击事件，延后执行（可传参写法，不限制参数数量）
       </n-button>
     </n-space>
   </naive-theme>
--- docs/utils/debounce/throttle.vue
+++ docs/utils/debounce/throttle.vue
@@ -9,6 +9,11 @@ const handle = () => {
 }
 
 const throttleClick = throttle(handle)
+
+const throttleClickParams: any = throttle((first, second) => {
+  message.success(first)
+  message.success(second)
+})
 </script>
 
 <template>
@@ -16,6 +21,9 @@ const throttleClick = throttle(handle)
     <n-space className="mt-2">
       <n-button @click="throttleClick">
         连续点击我，每一秒只会执行一次点击事件
+      </n-button>
+      <n-button @click="throttleClickParams('我是传过来的第一个参数', '我是传过来的第二个参数')">
+        连续点击我，每一秒只会执行一次点击事件（可传参写法，不限制参数数量）
       </n-button>
     </n-space>
   </naive-theme>
