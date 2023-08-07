7adbe45dff6c6139e088dd7e7ed85ae40252272d
xiaoxian521 <1923740402@qq.com>
2023-08-07 11:56:39 +0800

    docs: update

--- docs/hooks/useGlobal/global.vue
+++ docs/hooks/useGlobal/global.vue
@@ -5,14 +5,14 @@ import { useGlobal } from "@pureadmin/utils"
 let globals = ref()
 const property: Record<string, any> = useGlobal()
 
-// 可以打印$echarts，是因为在 https://github.com/xiaoxian521/pure-admin-utils-docs/blob/master/docs/.vitepress/plugins/echarts.ts#L4 这里将其挂载到全局实例上
+// 可以打印$echarts，是因为在 https://github.com/pure-admin/pure-admin-utils-docs/blob/master/docs/.vitepress/plugins/echarts.ts#L4 这里将其挂载到全局实例上
 </script>
 
 <template>
   <naive-theme>
     <n-space className="flex items-center">
       <n-button @click="globals = Object.keys(property)"> 获取 </n-button>
-      <n-button text tag="a" href="https://vuejs.org/api/application.html#app-config-globalproperties" target="_blank"
+      <n-button text tag="a" href="https://cn.vuejs.org/api/application.html#app-config-globalproperties" target="_blank"
         type="primary">
         点击了解全局property详情
       </n-button>
