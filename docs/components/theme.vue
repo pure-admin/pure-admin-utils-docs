7708f1f97f7e834099a36a67f572f8e5fc624988
xiaoxian521 <1923740402@qq.com>
2023-12-08 22:48:19 +0800

    chore: update

--- docs/components/theme.vue
+++ docs/components/theme.vue
@@ -1,16 +1,18 @@
 <script setup lang="ts">
-import { useDark } from "@pureadmin/utils";
+import { useData } from "vitepress";
 import { darkTheme } from "naive-ui";
 
 import hljs from "highlight.js/lib/core";
 import javascript from "highlight.js/lib/languages/javascript";
 hljs.registerLanguage("javascript", javascript);
 
-const { isDark } = useDark();
+const { isDark } = useData();
 </script>
 
 <template>
   <n-config-provider :theme="isDark ? darkTheme : undefined" :hljs="hljs">
-    <slot :dark="isDark" />
+    <ClientOnly>
+      <slot :dark="isDark" />
+    </ClientOnly>
   </n-config-provider>
 </template>
