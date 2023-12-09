7031a298a16369519e13676bbd20a1a77788659c
xiaoxian521 <1923740402@qq.com>
2023-12-09 19:13:36 +0800

    chore: update

--- docs/components/theme.vue
+++ docs/components/theme.vue
@@ -1,18 +1,33 @@
 <script setup lang="ts">
 import { useData } from "vitepress";
 import { darkTheme } from "naive-ui";
+import { ref, watch, onMounted } from "vue";
 
 import hljs from "highlight.js/lib/core";
 import javascript from "highlight.js/lib/languages/javascript";
 hljs.registerLanguage("javascript", javascript);
 
+let show = ref(false);
+let dark = ref(false);
 const { isDark } = useData();
+
+onMounted(() => {
+  dark.value = isDark.value;
+  show.value = true;
+});
+
+watch(
+  () => isDark.value,
+  val => (dark.value = val)
+);
 </script>
 
 <template>
-  <n-config-provider :theme="isDark ? darkTheme : undefined" :hljs="hljs">
-    <ClientOnly>
-      <slot :dark="isDark" />
-    </ClientOnly>
+  <n-config-provider
+    v-show="show"
+    :theme="dark ? darkTheme : undefined"
+    :hljs="hljs"
+  >
+    <slot :dark="dark" />
   </n-config-provider>
 </template>
