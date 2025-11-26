6540517adfd785ca217d93e9f9e73878a18e13f9
xiaoxian521 <1923740402@qq.com>
2025-11-26 18:41:16 +0800

    docs: 同步`2.6.3`版本，优化`getSvgInfo`函数

--- docs/utils/svg/types/getSvgInfo.ts
+++ docs/utils/svg/types/getSvgInfo.ts
@@ -3,6 +3,6 @@ export interface SvgInfo {
   width: number;
   /** SVG的高度，基于`viewBox`属性的第四个值 */
   height: number;
-  /** SVG中所有`<path>`元素的`outerHTML`连接而成的字符串 */
+  /** `<svg>` 元素内部的所有 HTML 内容（即 `innerHTML`），包含所有子元素如 `<path>`, `<g>`, `<rect>` 等 */
   body: string;
 };
